ad2fec48b0b27ec8e1d64b4999e16b1b1634593e
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-29 03:15:02 +0300

    Create bcu.js

---
+++ bcu.js
@@ -0,0 +1,161 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+  console.log('---------------');
+
+  console.log(' Gold Bot Is OnMusic')
+
+  console.log('---------------')
+
+  client.user.setStatus("Online")
+
+ client.user.setActivity(" United Server",{type: 'PLAYING'})
+
+});
+
+client.on('message', message => {
+
+   if(!message.channel.guild) return;
+
+if(message.content.startsWith( '$bc')) {
+
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+
+let BcList = new Discord.RichEmbed()
+
+.setThumbnail(message.author.avatarURL)
+
+.setAuthor(`محتوى الرساله ${args}`)
+
+.setDescription(`برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست\nيمكنك اضافة اسم السيرفر في البرودكاست عن طريق كتابة <server>\nيمكنك اضافة اسم المرسل في البرودكاست عن طريق كتاية <by>\nيمكنك منشنة العضو في الرساله عن طريق كتابة <user>`)
+
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+
+msg.react('📝')
+
+.then(() => msg.react('✏'))
+
+.then(() =>msg.react('📝'))
+
+ 
+
+let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+
+let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+
+ 
+
+let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+
+let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+
+ 
+
+ 
+
+EmbedBc.on("collect", r => {
+
+ 
+
+message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+
+message.guild.members.forEach(m => {
+
+let EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+
+var bc = new
+
+Discord.RichEmbed()
+
+.setColor('RANDOM')
+
+.setDescription(EmbedRep)
+
+.setThumbnail(message.author.avatarURL)
+
+m.send({ embed: bc })
+
+msg.delete();
+
+})
+
+})
+
+NormalBc.on("collect", r => {
+
+  message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+
+message.guild.members.forEach(m => {
+
+let NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+
+m.send(NormalRep);
+
+msg.delete();
+
+})
+
+})
+
+})
+
+}
+
+});
+
+const adminprefix = "$vip";//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+
+const devs = ['470500980342128650'];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+
+client.on('message', message => {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+
+  var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+
+    if (!devs.includes(message.author.id)) return;//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+
+    
+
+if (message.content.startsWith(adminprefix + 'setgame')) {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+
+  client.user.setGame(argresult);
+
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+
+} else 
+
+  if (message.content.startsWith(adminprefix + 'setname')) {
+
+client.user.setUsername(argresult).then
+
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+
+} else
+
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+
+client.user.setAvatar(argresult);
+
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+
+      } else     
+
+if (message.content.startsWith(adminprefix + 'setT')) {
+
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+
+}
+
+});
+
+client.login(process.env.BOT_TOKEN); 

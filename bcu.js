664eb271508f65c29baaafae907868a871641806
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-31 17:36:59 +0300

    Update bcu.js

--- bcu.js
+++ bcu.js
@@ -164,16 +164,66 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 
 
-    
-
-    
-
-    
-
-    
-        
-         
+          
             
+
+  const misaka = new Set();
+
+    client.on('message', async msg => {
+
+  if(msg.content.startsWith("رابط")) {
+
+  if (misaka.has(msg.author.id)) {
+
+    let misakaemb = new Discord.RichEmbed()
+
+    .setDescription(`يجب عليك الانتظار 24 ساعه!`)
+
+    .setColor(`RED`)
+
+    return msg.channel.send(misakaemb).then(message => {
+
+     message.delete(10000) 
+
+    })
+
+    
+
+    }
+
+    misaka.add(msg.author.id);
+
+  
+
+   msg.channel.createInvite({
+
+        thing: true,
+
+        maxUses: 5,
+
+        maxAge: 86400,
+
+  }).then(invite =>
+
+   msg.author.sendMessage(`
+
+         <@${msg.author.id}> 
+
+         **maxUses: 5 **
+
+         ${invite.url}`)
+
+  )
+
+    msg.channel.send(`**:link: Invite Linke Sent In DM Successfully**`)
+
+  }
+
+    setTimeout(() => {
+
+    },86400000);
+
+    })          
 
             
 
@@ -185,77 +235,312 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
            
 
-
-
-        
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-
-
-
-                                 
- 
-
- 
-
-     
-
-     
-
-                    
-
-     
-
-                   
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
- 
-
- 
-
+client.on('message',message =>{
 
     
 
-    
+    if(message.content.startsWith(prefix + 'top')) {
 
-            
+  message.guild.fetchInvites().then(i =>{
 
-      
-        
-      
+  var invites = [];
 
-      
-      
+   
+
+  i.forEach(inv =>{
+
+    var [invs,i]=[{},null];
+
      
+
+    if(inv.maxUses){
+
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+
+    }else{
+
+        invs[inv.code] =+ inv.uses;
+
+    }
+
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+
+   
+
+  });
+
+  var embed = new Discord.RichEmbed()
+
+  .setColor("#000000")
+
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+
+  
+
+           message.channel.send({ embed: embed });
+
+   
+
+  });
+
+   
+
+    }
+
+  });
+
+  
+
+  
+
+client.on('message', message => {
+
+if (message.content.startsWith('#inv-info')) {
+
+let oi = message.mentions.users.first() ? message.mentions.users.first().id : message.author.id ; 
+
+  let img = message.mentions.users.first() ? message.mentions.users.first().username : message.author.username;
+
+  let imagemm = message.mentions.users.first() ? message.mentions.users.first().avatarURL : message.author.avatarURL
+
+  message.guild.fetchInvites().then(invs => {
+
+    let member = client.guilds.get(message.guild.id).members.get(oi);
+
+    let personalInvites = invs.filter(i => i.inviter.id === oi);
+
+    let urll = invs.filter(i => i.inviter.id === oi);
+
+    let link = urll.reduce((p , v) => v.url +` , Total de membros recrutados no convite: ${v.uses}.\n`+ p, `\nServidor: ${message.guild.name} \n `);
+
+    let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+
+   let exec = personalInvites.reduce((p, v) => v.inviter);
+
+ let possibleInvites = [['Total de membros recrutados:']];
+
+possibleInvites.push([inviteCount, exec]);
+
+        let user = message.mentions.users.first() || message.author;
+
+        let mem = message.guild.member(user);
+
+        let millisJoined = new Date().getTime() - mem.joinedAt.getTime();
+
+        let daysJoined = millisJoined / 1000 / 60 / 60 / 24;
+
+const alpha = new Discord.RichEmbed()
+
+.setAuthor(img)
+
+.addField('🏆 Invite Infos',  `\n\n► لقد قمت بدعوة ما مجموعه \`\`${Number(inviteCount)}\`\` عضو.\n\n► لقد انضممت لسرفر مند\`${daysJoined.toFixed(0)}\`يوم .\n\n► لقد انضممت بهذه الدعوة\`${exec}\``,true)
+
+.setThumbnail(imagemm)
+
+.setColor(0x4959e9);
+
+message.channel.send(alpha);
+
+});
+
+};
+
+  });
+
+       
+
+
+var user = {};
+var warn = {};
+
+var user = {};
+var warn = {};
+
+client.on('message', function(message) {
+
+    	 if (!message.channel.guild) return;
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+
+  if (message.author.id == client.user.id) return;
+  if(JSON.stringify(user).indexOf(message.author.id) == -1) {
+    user[message.author.id] = message.createdTimestamp;
+    return;
+  } else {
+    if (Date.now() - user[message.author.id] < 695){
+              message.author.delete
+
+      if (JSON.stringify(warn).indexOf(message.author.id) == -1) {
+        warn[message.author.id] = 1;
+      } else {
+        warn[message.author.id]++;
+        message.author.delete
+      }
+      if (warn[message.author.id] < 8) {
+        message.author.delete
+
+      }
+      delete user[message.author.id];
+              message.author.delete
+
+    } else {
+      delete user[message.author.id];
+              message.author.delete
+
+    }
+  }
+  if (warn[message.author.id] == 8) {		   
+     if (!message.channel.guild) return;
+             message.author.delete
+
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+          var currentTime = new Date(),
+                   Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate(),
+hours = currentTime.getHours() + 3 ,
+            minutes = currentTime.getMinutes()+1,
+            seconds = currentTime.getSeconds();
+
+           if (!message.channel.guild) return;
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+    message.guild.members.get(message.author.id).addRole(muteRole1);
+    
+     var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+
+delete warn[message.author.id];
+    delete user[message.author.id];
+	const embed500 = new Discord.RichEmbed()
+     .setTitle(`المرسل ${message.author.username}#${message.author.discriminator} `)
+      .setDescription(":white_check_mark:  | `محاولة السبام`\n\nالاسم:\n"+`${message.author.username}#${message.author.discriminator}`+"\nالعقوبة:\nميوت شات\n")
+          .setFooter("Crystal")
+      .setColor("c91616")
+    message.channel.send(embed500)
+    	const embed20 = new Discord.RichEmbed()
+      .setTitle(":scales: | تمت معاقبتك")
+      .setDescription(`**لقد قمت بمخالفة قوانين السيرفر**\n\nتمت معاقبتك من قبل :\nL-\nنوع العقوبة:\nميوت شات\nتاريخ العقوبة:\n`+ Year + "/" + Month + "/" + Day +', '+hours +'-' +minutes+'-'+seconds+"\n \n \n`في حال كانت العقوبة بالغلط, تواصل مع الادارة`")
+          .setFooter("Crystal")
+      .setColor("c91616")
+    
+     message.author.send(embed20)
+  
+  }
+});
+
+
+
+
+
+client.on('ready', () => {
+
+});
+/*جميع الح��وق محفوظهه لريبل ولسيرفر كودز
+رآح يرسل للأونر تحذير + م يتطلب ملفات سويته لكم داتا مؤقت
+سو روم بأسم log 
+أو غيره من الكود عشان يرسل هنا التحذير
+nvm i 10 
+nvm use 10
+npm i discord.js
+*/
+var guilds = {};
+client.on('guildBanAdd', function(guild) {
+            const rebellog = client.channels.find("name", "log"),
+            Onumber = 3,
+  Otime = 10000
+guild.fetchAuditLogs({
+    type: 22
+}).then(audit => {
+    let banner = audit.entries.map(banner => banner.executor.id)
+    let bans = guilds[guild.id + banner].bans || 0 
+    guilds[guild.id + banner] = {
+        bans: 0
+    }
+      bans[guilds.id].bans += 1; 
+if(guilds[guild.id + banner].bans >= Onumber) {
+try {
+let roles = guild.members.get(banner).roles.array();
+guild.members.get(banner).removeRoles(roles);
+  guild.guild.member(banner).kick();
+
+} catch (error) {
+console.log(error)
+try {
+guild.members.get(banner).ban();
+  rebellog.send(`<@!${banner.id}>
+حآول العبث بالسيرفر @everyone`);
+guild.owner.send(`<@!${banner.id}>
+حآول العبث بالسيرفر ${guild.name}`)
+    setTimeout(() => {
+ guilds[guild.id].bans = 0;
+  },Otime)
+} catch (error) {
+console.log(error)
+}
+}
+}
+})
+});
+ let channelc = {};
+  client.on('channelCreate', async (channel) => {
+  const rebellog = client.channels.find("name", "log"),
+  Oguild = channel.guild,
+  Onumber = 3,
+  Otime = 10000;
+  const audit = await channel.guild.fetchAuditLogs({limit: 1});
+  const channelcreate = audit.entries.first().executor;
+  console.log(` A ${channel.type} Channel called ${channel.name} was Created By ${channelcreate.tag}`);
+   if(!channelc[channelcreate.id]) {
+    channelc[channelcreate.id] = {
+    created : 0
+     }
+ }
+ channelc[channelcreate.id].created += 1;
+ if(channelc[channelcreate.id].created >= Onumber ) {
+    Oguild.members.get(channelcreate.id).kick();
+rebellog.send(`<@!${channelcreate.id}>
+حآول العبث بالسيرفر @everyone`);
+channel.guild.owner.send(`<@!${channelcreate.id}>
+حآول العبث بالسيرفر ${channel.guild.name}`)
+}
+  setTimeout(() => {
+ channelc[channelcreate.id].created = 0;
+  },Otime)
+  });
+
+let channelr = {};
+  client.on('channelDelete', async (channel) => {
+  const rebellog = client.channels.find("name", "log"),
+  Oguild = channel.guild,
+  Onumber = 3,
+  Otime = 10000;
+  const audit = await channel.guild.fetchAuditLogs({limit: 1});
+  const channelremover = audit.entries.first().executor;
+  console.log(` A ${channel.type} Channel called ${channel.name} was deleted By ${channelremover.tag}`);
+   if(!channelr[channelremover.id]) {
+    channelr[channelremover.id] = {
+    deleted : 0
+     }
+ }
+ channelr[channelremover.id].deleted += 1;
+ if(channelr[channelremover.id].deleted >= Onumber ) {
+  Oguild.guild.member(channelremover).kick();
+rebellog.send(`<@!${channelremover.id}>
+حآول العبث بالسيرفر @everyone`);
+channel.guild.owner.send(`<@!${channelremover.id}>
+حآول العبث بالسيرفر ${channel.guild.name}`)
+}
+  setTimeout(() => {
+ channelr[channelremover.id].deleted = 0;
+  },Otime)
+  });
+
+
+
+  
 client.login(process.env.BOT_TOKEN); 

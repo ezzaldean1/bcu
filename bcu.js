9146e3a2826c36cc8536647a3aa57a50c799709c
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-31 03:35:38 +0300

    Update bcu.js

--- bcu.js
+++ bcu.js
@@ -230,59 +230,9 @@ const misaka = new Set();
 
            
 
-client.on('message',message =>{
 
-    
 
-    if(message.content.startsWith(prefix + 'top')) {
-
-  message.guild.fetchInvites().then(i =>{
-
-  var invites = [];
-
-   
-
-  i.forEach(inv =>{
-
-    var [invs,i]=[{},null];
-
-     
-
-    if(inv.maxUses){
-
-        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
-
-    }else{
-
-        invs[inv.code] =+ inv.uses;
-
-    }
-
-        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
-
-   
-
-  });
-
-  var embed = new Discord.RichEmbed()
-
-  .setColor("#000000")
-
-  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-
-  
-
-           message.channel.send({ embed: embed });
-
-   
-
-  });
-
-   
-
-    }
-
-  });
+        
 
   
 
